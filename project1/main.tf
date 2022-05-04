1bcebd1c59e47e056aadddaaf729bf9bc0dd90cd
Jeff Bonhag <jbonhag@hashicorp.com>
2022-05-04 13:26:00 -0400

    un-interpolate values

--- project1/main.tf
+++ project1/main.tf
@@ -1,6 +1,6 @@
 resource "random_pet" "server1" {
   keepers = {
     # Generate a new pet name each time we switch to a new AMI id
-    ami_id = "${var.project1_ami_id}"
+    ami_id = var.project1_ami_id
   }
 }
